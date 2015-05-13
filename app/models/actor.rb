cd4a12ac5a2e26cd4a4e0b71ef4117da216d2f09
Arjun Venkataswamy <avenkat2@gmail.com>
2015-05-12 21:44:28 -0500

    starting point for movies to watch sign in

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,26 @@
 class Actor < ActiveRecord::Base
+  def roles
+    Role.where(:actor_id => self.id)
+  end
+
+  def movies
+    movies = []
+    self.roles.each do |role|
+      movies << role.movie
+    end
+    return movies
+  end
+
+  def movies_refactored
+    self.roles.map { |role| role.movie }
+  end
+
+  def movies_refactored_explained
+    # temp_array = []
+    self.roles.map do |role|
+      role.movie
+      # temp_arry < role.movie
+      # return temp_array
+    end
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,16 @@ class Movie < ActiveRecord::Base
   def director
     Director.find(self.director_id)
   end
+
+  def roles
+    Role.where(:movie_id => self.id)
+  end
+
+  def actors
+    actors = []
+    self.roles.each do |role|
+      actors << role.actor
+    end
+    return actors
+  end
 end
